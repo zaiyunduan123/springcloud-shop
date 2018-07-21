bfb1cd153b6aa3a9b680a08d49bf091e53706e83
zaiyunduan123 <1778667492@qq.com>
2018-07-21 10:29:33 +0800

    docs ： docker

--- order/src/main/java/com/jesper/OrderApplication.java
+++ order/src/main/java/com/jesper/OrderApplication.java
@@ -2,12 +2,17 @@ package com.jesper;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@SpringBootApplication
-@EnableDiscoveryClient
 @EnableFeignClients
+
+//@SpringBootApplication
+//@EnableDiscoveryClient
+//@EnableCircuitBreaker
+@SpringCloudApplication
 public class OrderApplication {
 
 	public static void main(String[] args) {
--- order/src/main/java/com/jesper/controller/HystrixController.java
+++ order/src/main/java/com/jesper/controller/HystrixController.java
@@ -1,6 +1,11 @@
 package com.jesper.controller;
 
+import com.netflix.hystrix.HystrixCommandProperties;
+import com.netflix.hystrix.contrib.javanica.annotation.DefaultProperties;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
@@ -12,12 +17,35 @@ import java.util.Arrays;
  * 使用Hystrix实现服务降级
  */
 @RestController
+@DefaultProperties(defaultFallback = "defaultFallback")
 public class HystrixController {
 
+//    @HystrixCommand(fallbackMethod = "fallback")//出故障会用到fallback
+
+    /**
+     * 熔断
+     * @return
+     */
+    @HystrixCommand(commandProperties = {
+            @HystrixProperty(name = "circuitBreaker.enabled", value = "true"),  //设置熔断
+            @HystrixProperty(name = "circuitBreaker.requestVolumeThreshold", value = "10"), //
+            @HystrixProperty(name = "circuitBreaker.sleepWindowInMilliseconds", value = "10000"),
+            @HystrixProperty(name = "circuitBreaker.errorThresholdPercentage", value = "60")
+
+    })
     @GetMapping("getProductInfoList")
-    public String getProductInfoList() {
+    public String getProductInfoList(@RequestParam("number") Integer number) {
         RestTemplate restTemplate = new RestTemplate();
         return restTemplate.postForObject("http://127.0.0.1:8080/product/listForOrder",
                 Arrays.asList("157875196366160022"), String.class);
+    }
+
+    //服务降级
+    private String fallback(){
+        return "太拥挤了，请稍后再试";
+    }
+
+    private String defaultFallback(){
+        return "默认提示太拥挤了，请稍后再试";
     }
 }
